71ec311c125efea27df649c0e16189f2fef13d04
apsdehal <apsdehal@gmail.com>
2014-04-24 00:43:01 +0530

    Creates code for muzi top20

--- www/js/index.js
+++ www/js/index.js
@@ -6,7 +6,16 @@ var muzi = {
 
     home: function(){
         $.getJSON('https://beta.sdslabs.co.in/muzi/ajax/track/top.php',{},function(data){
-            $(".content").html(data);
+            var html = '';
+            html += '<ul data-role="listview" data-inset="true" data-filter="true">'
+            for(i in data){
+                html += '<li><div>' + data[i].title + '</div>\
+                        <div>' + data[i].artist + '</div>\
+                        <div>' + data[i].album + '</div>\
+                        </li>';
+            }
+            html += '</ul>';
+            $(".content").html(html);
         });
     },
 };
