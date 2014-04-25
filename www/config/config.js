d94a50b2a3647e337c7ad57272ceca6b15e17ea6
apsdehal <apsdehal@gmail.com>
2014-04-25 13:31:27 +0530

    Initial code for starting up howler

--- www/config/config.js
+++ www/config/config.js
@@ -1,12 +1,12 @@
 /**
  * The configuration options go here
- * @class muzi
+ * @class crunch
  */
-muzi = {
+crunch = {
 	config : {
 		musicRoot:"https://music.sdslabs.co.in/",
 		ajaxRoot: "https://beta.sdslabs.co.in/muzi/ajax/",
-		bandRoot: "https://sdslabs.co.in/muzi/artist_pics/",
+		bandRoot: "https://sdslabs.co.in/crunch/artist_pics/",
 		picsRoot: 'https://cdn.sdslabs.co.in/music_pics/',
 		lastfm: 'abe9e5043c73cff162b13c0215680aeb',
 	}
--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,11 @@
 crunch = crunch || {};
 crunch = $.extend(crunch, {
 
+    current: {
+        nowPlaying: false,
+        data: null
+    },
+
     initialize: function(){
         crunch.home();
         crunch.hooks();
@@ -14,16 +19,20 @@ crunch = $.extend(crunch, {
 
     play: function(id){
         $.get('track/',{id:id}, function(data){
+            crunch.current.data = data;
             var file = crunch.config.music + data.file.split('/').map(function(x){return encodeURIComponent(x);}).join('/');
-
+            crunch.current.nowPlaying = new Howl({
+                urls: [file],
+                buffer: true,
+            }).play();
         });
-    }
+    },
 
     loadContent: function(data){
         var html = '';
             html += '<ul data-role="listview" data-inset="true" data-filter="true">'
             for(i in data){
-                html += '<li class="songLi" mid="' + data[i].id + '"><div>' + data[i].title + '</div>\
+                html += '<li class="trackLi" data-mid="' + data[i].id + '"><div>' + data[i].title + '</div>\
                         <div>' + data[i].artist + '</div>\
                         </li>';
             }
@@ -41,8 +50,13 @@ crunch = $.extend(crunch, {
     },
 
     hooks: function(){
-        $(".main").delegate('#search_text','keyup',function(e){
+        var mainHandle = $(".main");
+        mainHandle.delegate('#search_text','keyup',function(e){
                 crunch.search.init(e.target.value);
         });
-    }
+
+        mainHandle.delegate('.trackLi', 'click', function(){
+            crunch.play($(this).attr('data-mid'));
+        });
+    },
 });
